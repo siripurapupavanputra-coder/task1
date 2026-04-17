b054cf04dbd0e0352ddaf514c3a52d4d79c33ec7
pavan putra <siripurapupavanputra@gmail.com>
2026-04-18 00:07:00 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+//task1
+var a = 50;
+var a = 100;
+console.log(a);
+
+//task2
+let marks = 100;
+console.log(marks+10)
+
+//task3
+const price = 500
+const final_price = price + 100
+console.log(final_price)
+
+//task4
+console.log("Welcome Team");
+console.log(2026);
+console.log(true);
+
+//task5
+console.log(typeof("JavaScript"))
+console.log(typeof(2025))
+console.log(typeof(false))
+
+//task6
+let fruits = ["apple","banana","grapes","mango","kiwi"]
+console.log(fruits)
+console.log(fruits[0]);
+console.log(fruits[fruits.length-1]);
+
+//task7
+student = {
+ name: "Naveen",
+ age: 20,
+ course: "MERN"
+}
+console.log(student.name)
+console.log(student.age)
+
+//task8
+console.log(20 + 10)
+console.log(50 - 25)
+console.log(5 * 5)
+console.log(100 / 4)
+console.log(20 % 3)
+
+//task9
+let x = 5;
+x++;
+console.log(x);
+
+let y = 10;
+y--;
+console.log(y);
+
+//task10
+console.log(10 > 5)
+console.log(5 < 2)
+console.log(20 == "20")
+console.log(15 === "15")
+console.log(10 != 8)
+
+//task11
+console.log(5 > 2 && 10 > 3)
+console.log(7 < 5 || 8 > 2)
+console.log(!(10 > 5))
+
+//task12
+let age = 18;
+//If age >= 18 print "Eligible" else "Not Eligible"
+if(age>=18){
+    console.log("eligible")
+}else{
+    console.log("Not eligible")
+}
